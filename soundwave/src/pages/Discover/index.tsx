4de3be8bcc8606070b693ba144b25225675cec87
Acrata <hxtr227@gmail.com>
2023-07-06 15:02:02 +0200

    React router conf

---
+++ soundwave/src/pages/Discover/index.tsx
@@ -0,0 +1,7 @@
+export const Discover = (props : {}) => {
+  return (
+    <div>
+      <h1>Discover</h1>
+    </div>
+  )
+}
---
+++ soundwave/src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+export const Home = (props : {}) => {
+  return (
+    <div>
+        <h1>Home here</h1>
+    </div>
+  )
+}
